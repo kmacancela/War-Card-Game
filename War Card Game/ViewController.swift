79a44c3b133391d3fa46eaa1083d3f426f7e6404
Karina Macancela <karymacancela@gmail.com>
2019-05-04 11:27:27 -0400

    Initial Commit

---
+++ War Card Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  War Card Game
+//
+//  Created by Karina Macancela on 5/4/19.
+//  Copyright © 2019 Karina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
